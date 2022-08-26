e08960c20a2dd98cb2b3f2ff70cfb5ba3faa2731
bhushanmahendrakar <112152346+bhushanmahendrakar@users.noreply.github.com>
2022-08-26 12:14:44 +0530

    Update Githubdemo1.java

--- github/src/github/demo1/Githubdemo1.java
+++ github/src/github/demo1/Githubdemo1.java
@@ -2,7 +2,7 @@ package github.demo1;
 
 public class Githubdemo1 {
 public static void main (String [] args ) {
-	System.out.println(" badabail pola");
+	System.out.println(" Badabail pola");
 	
 }
 }
